0916d9fc700278cdede328c52a9e9995d6f42eee
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-05-12 17:41:35 -0700

    add new area form

---
+++ app/controllers/areas_controller.rb
@@ -0,0 +1,28 @@
+class AreasController < ApplicationController
+  def index
+    @areas = Area.all
+  end
+
+  def show
+    @area = Area.find(params[:id])
+  end
+
+  def create
+    @area = Area.new
+  end
+
+  def new
+    @area = Area.new
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+    def areas_params
+      params.require(:area).permit!
+    end
+end
--- app/models/area.rb
+++ app/models/area.rb
@@ -1,2 +1,9 @@
 class Area < ActiveRecord::Base
+  has_many :photos
+
+  validates :display_name, uniqueness: { case_sensitive: false }, presence: true
+  validates :code, uniqueness: { case_sensitive: false }, presence: true, length: {is: 3}
+  validates :short_intro, length: {maximum: 90}
+  validates :description, length: {maximum: 400}
+
 end
