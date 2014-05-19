0294ae5a49a87069cec33ac98de528656c8e3362
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-05-18 18:19:36 -0700

    add devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513230418) do
+ActiveRecord::Schema.define(version: 20140516185125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,5 +40,12 @@ ActiveRecord::Schema.define(version: 20140513230418) do
   end
 
   add_index "photos", ["area_id"], name: "index_photos_on_area_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
