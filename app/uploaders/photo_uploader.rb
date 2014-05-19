d74d401fab7662efaf26b12fae5a564521ea90c3
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-05-19 16:05:41 -0700

    add resrc-it for image responsiveness and clean up css

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -25,24 +25,24 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_fit => [2532, 1876]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  version :mobile do
-    process :resize_to_fit => [50, 50]
-  end
+  # version :mobile do
+  #   process :resize_to_fit => [50, 50]
+  # end
 
-  version :tablet do
-    process :resize_to_fit => [300, 300]
-  end
+  # version :tablet do
+  #   process :resize_to_fit => [300, 300]
+  # end
 
-  version :desktop do
-    process :resize_to_fit => [800, 800]
-  end
+  # version : do
+  #   process :resize_to_fit => [800, 800]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
