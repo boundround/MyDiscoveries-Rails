830ea9dd102c4eb3cbb95cd47bc972d1a369bb45
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-05-20 18:27:29 -0700

    add required fields to add new area form

--- app/models/area.rb
+++ app/models/area.rb
@@ -4,7 +4,7 @@ class Area < ActiveRecord::Base
 
   validates :display_name, uniqueness: { case_sensitive: false }, presence: true
   validates :code, uniqueness: { case_sensitive: false }, presence: true, length: {is: 3}
-  validates :short_intro, length: {maximum: 90}
-  validates :description, length: {maximum: 400}
+  validates :short_intro, length: {maximum: 90}, presence: true
+  validates :description, length: {maximum: 400}, presence: true
 
 end
