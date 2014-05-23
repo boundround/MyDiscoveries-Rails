7be0be0385483c02e3123d91d54960e0d45de00e
Gilad Salmon <Gilad.Salmon@gmail.com>
2014-05-23 09:24:25 -0700

    Modify map to cover entire page, have bounds, bg color to match, etc

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,14 +1,16 @@
-map = L.mapbox.map('map', 'gilupstream.i8lde4kj', {
+map = L.mapbox.map('map', 'oztexan.map-m8oepi17', {
     // these options apply to the tile layer in the map
     tileLayer: {
         // this map option disables world wrapping. by default, it is false.
         continuousWorld: false,
         // this option disables loading tiles outside of the world bounds.
         noWrap: true
-      }
+      },
+    maxBounds: [[85,-180],[-85,180]],
+    maxZoom: 2
     });
 
-// oztexan.map-m8oepi17
+// gilupstream.i8lde4kj
 
 $.ajax({
   dataType: 'text',
