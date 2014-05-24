6cfff265f8cf9986b8f59d98c82a2ee56728a4ac
Gilad Salmon <Gilad.Salmon@gmail.com>
2014-05-24 13:31:33 -0700

    Implement marker clustering

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -6,20 +6,50 @@ map = L.mapbox.map('map', 'oztexan.map-m8oepi17', {
         // this option disables loading tiles outside of the world bounds.
         noWrap: true
       },
+    zoomControl: false,
     maxBounds: [[85,-180],[-85,180]],
-    maxZoom: 2
+    minZoom: 2,
+    maxZoom: 15
     });
+
+L.control.zoomslider().addTo(map);
 
 // gilupstream.i8lde4kj
 
+var areaLayer = L.mapbox.featureLayer().addTo(map);
+
+var addMarkers = function(geojson, layer) {
+  layer.setGeoJSON(window.geojson);
+};
+
 $.ajax({
-  dataType: 'text',
   url: '/areas.json',
   success: function(data) {
-    var geojson;
-    geojson = $.parseJSON(data);
-    var featureLayer = L.mapbox.featureLayer(geojson)
-    .addTo(map);
+    window.geojson = data;
+    // addMarkers(data, areaLayer);
+    markClust(data);
   }
 });
 
+areaLayer.on('click', function(e) {
+    $('#myModal').modal({remote: '/areas/1.html', show: 'true'});
+});
+
+
+// markecluster code
+
+
+var markers = new L.MarkerClusterGroup();
+
+var markClust = function (geojson) {
+  for (var i = 0; i < geojson.length; i++) {
+    var a = geojson[i];
+    var marker = L.marker(new L.LatLng(a.geometry.coordinates[1], a.geometry.coordinates[0]), {
+        icon: L.mapbox.marker.icon({'marker-symbol': 'star', 'marker-color': '0044FF'}),
+    });
+    markers.addLayer(marker);
+}
+
+map.addLayer(markers);
+
+};
