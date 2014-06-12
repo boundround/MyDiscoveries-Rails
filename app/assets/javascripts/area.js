2e5b302f4af66b977b06e03a37d9a44ad2487a40
Gilad Salmon <Gilad.Salmon@gmail.com>
2014-06-12 10:59:48 -0700

    Add masonry to area photos tab

--- app/assets/javascripts/area.js
+++ app/assets/javascripts/area.js
@@ -7,10 +7,14 @@ $("#pac-input").geocomplete({
 
 $('#showAreaModal').modal();
 
-
 $(document).ready(function(){
   $('.fun-fact-icon').on('click', function() {
     $(this).next('.fun-fact').toggle("slow");
+  });
+
+  var msnry = new Masonry( '#photos-masonry', {
+  columnWidth: 100,
+  itemSelector: '.photo'
   });
 });
 
