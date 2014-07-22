436b23c61cbc35777953d8573dc4e41dcd8abd38
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-07-22 16:27:15 -0700

    add id back to join table

--- db/migrate/20140717174937_create_categorizations.rb
+++ db/migrate/20140717174937_create_categorizations.rb
@@ -1,6 +1,6 @@
 class CreateCategorizations < ActiveRecord::Migration
   def change
-    create_table :categorizations, id: false do |t|
+    create_table :categorizations do |t|
       t.integer :category_id
       t.integer :place_id
 
