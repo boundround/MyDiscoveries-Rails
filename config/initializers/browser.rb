41ca886e894a0db295704205e8fb89f659d1e820
Gilad Salmon <Gilad.Salmon@gmail.com>
2014-07-29 10:33:56 -0700

    Move filter into a partial, fix marker issues on unsupported browsers, add unsupported warning in a modal

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,4 +1,6 @@
 Rails.configuration.middleware.use Browser::Middleware do
-  redirect_to '/map' unless browser.modern?
-  redirect_to '/map' if browser.android? && browser.version.to_i < 4 
+  if request.path == '/'
+    redirect_to '/map' unless browser.modern?
+    redirect_to '/map' if browser.android? && browser.version.to_i < 4
+  end
 end
