bc910b5b07283228a4eb2dc3b6a7f7bac96c1c32
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-07-29 13:28:53 -0700

    add published status to areas, add radio buttons to area new and edit forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140724234350) do
+ActiveRecord::Schema.define(version: 20140729195119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140724234350) do
     t.datetime "updated_at"
     t.string   "address"
     t.string   "slug"
+    t.string   "published_status"
   end
 
   add_index "areas", ["slug"], name: "index_areas_on_slug", using: :btree
