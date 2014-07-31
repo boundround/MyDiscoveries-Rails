983de626998d486fbc39b5bcc4dfdd107329d6d2
Gilad Salmon <Gilad.Salmon@gmail.com>
2014-07-31 11:05:46 -0700

    Redirect all mobile traffic to 2d map

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,6 +1,5 @@
 Rails.configuration.middleware.use Browser::Middleware do
   if request.path == '/'
-    redirect_to map_path unless browser.modern?
-    redirect_to map_path if browser.android? && browser.version.to_i < 4
+    redirect_to map_path unless browser.modern? && !browser.mobile?
   end
 end
