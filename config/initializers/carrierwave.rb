f88fe1c347aac3606a757f45c8f72a772bfc4aa0
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-07-31 16:22:48 -0700

    change rackspace to aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,13 @@
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
-    provider:           "Rackspace",
-    rackspace_username: ENV['RACKSPACE_USERNAME'],
-    rackspace_api_key:  ENV['RACKSPACE_KEY'],
-    rackspace_region:   :syd
+    provider:           "AWS",
+    aws_access_key_id: ENV['AS3_ACCESS_KEY'],
+    aws_secret_access_key:  ENV['AS3_SECRET_ACCESS_KEY']
   }
-  config.fog_directory = ENV['RACKSPACE_DIRECTORY']
-  config.asset_host    = ENV['RACKSPACE_HOST']
+
+  config.fog_directory = ENV['AS3_BUCKET_NAME']
+
+  config.asset_host = "http://d1w99recw67lvf.cloudfront.net"
+  config.fog_public = true
 end
