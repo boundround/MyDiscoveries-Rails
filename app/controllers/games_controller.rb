9f8174734c2ef56c21b79b469fe620a2327e2c67
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-08-07 17:28:59 -0700

    add radio buttons for game type

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -29,7 +29,7 @@ class GamesController < ApplicationController
 
   def update
     @game = Game.find(params[:id])
-    if @game.update(photo_params)
+    if @game.update(game_params)
       redirect_to :back
     end
   end
@@ -42,7 +42,7 @@ class GamesController < ApplicationController
   private
 
     def game_params
-      params.require(:game).permit(:url, :area_id, :place_id, :priority)
+      params.require(:game).permit(:url, :area_id, :place_id, :priority, :game_type)
     end
 
 end
