032bcddd54d15545548ebe37aaeef159797fda71
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-08-07 23:45:27 -0700

    remove document ready from char count js

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,6 +1,4 @@
-$(document).ready(function() {
-  $('.caption').keyup(function(){
-    var len = $(this).val().length;
-    $(this).closest('.control-group').next('.charCount').text(140 - len);
-  });
+$('.caption').keyup(function(){
+  var len = $(this).val().length;
+  $(this).closest('.control-group').next('.charCount').text(140 - len);
 });
