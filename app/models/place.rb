df34c0128d16770e159baf91239150c1988f301a
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-08-11 11:21:48 -0700

    check for nil url for geoJson

--- app/models/place.rb
+++ app/models/place.rb
@@ -25,7 +25,7 @@ class Place < ActiveRecord::Base
       # Assign icon based on 'premium' level and category
       icon_file_name = map_icon_for(place.categories[0].identifier)
 
-      if place.subscription_level == "Premium" && place.map_icon
+      if place.subscription_level == "Premium" && place.map_icon.url
         icon_file_name = place.map_icon.url.gsub('http://d1w99recw67lvf.cloudfront.net/vector_icons/', '').gsub(/svg/, 'png')
       end
 
