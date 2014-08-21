a9cd8c91a2329f51d35a974da17007cbe0c5e931
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-08-20 17:19:05 -0700

    remove script in show

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+window.onload(function(){
   $('.caption').on("keypress", function(){
       var len = $(this).val().length;
       $(this).closest('.control-group').next('.charCount').text(140 - len);
