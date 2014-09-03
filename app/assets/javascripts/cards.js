c8bb3d6a9c0b93dbfa0b10b11c6c7c359e70289d
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-09-03 14:49:28 -0700

    add feedback link to bottom of area and places show

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-  $('.caption').on("keypress", function(){
-      var len = $(this).val().length;
-      $(this).closest('.control-group').next('.charCount').text(140 - len);
-  });
 
   var loadIsotope = function() {
     var $container = $('#photos-masonry');
