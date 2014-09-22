0808a274904129d8e776ce648936b285082ae20a
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-09-22 16:28:38 -0700

    == not !=

--- app/models/area.rb
+++ app/models/area.rb
@@ -28,7 +28,7 @@ class Area < ActiveRecord::Base
           "title"=> area.display_name,
           "url" => '/areas/' + area.slug + '.html',
           "id" => area.id,
-          "places" => !area.places.all? { |place| place.subscription_level != "draft" },
+          "places" => !area.places.all? { |place| place.subscription_level == "draft" },
           "icon" => {
             "iconUrl" => 'https://s3.amazonaws.com/donovan-bucket/orange_plane.png',
             # size of the icon
