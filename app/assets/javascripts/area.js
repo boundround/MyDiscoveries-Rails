04f19b17bd9e7154bf4fdb2df28343b9cbe50982
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-10-07 13:51:09 -0700

    add cookie for initial nav modal

--- app/assets/javascripts/area.js
+++ app/assets/javascripts/area.js
@@ -4,3 +4,12 @@ $("#pac-input").geocomplete({
   details: "body",
   detailsAttribute: "data-geo"
 });
+
+$(document).ready(function() {
+  if ($.cookie('modal_shown') == null) {
+      $.cookie('modal_shown', 'yes');
+      $('#navModal').modal('show');
+  } else {
+    $('#navModal').modal('hide');
+  }
+});
