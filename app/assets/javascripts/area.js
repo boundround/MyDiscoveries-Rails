2edde8f8cbd10f4011710021cd16690e8bd68aca
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-11-21 13:48:19 -0800

    add map to edit and new places

--- app/assets/javascripts/area.js
+++ app/assets/javascripts/area.js
@@ -1,6 +1,13 @@
 $("#pac-input").geocomplete({
   types: ['geocode', 'establishment'],
   map: ".map-canvas",
+  markerOptions: {
+    draggable: true
+  },
   details: "body",
-  detailsAttribute: "data-geo"
+  detailsAttribute: "data-geo",
 });
+
+var geoValue = $("#place_address").val();
+
+$("#pac-input").geocomplete("find", geoValue);
