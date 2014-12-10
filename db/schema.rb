cacb302f6d8785f0a49a47b5a6bfd1f0bf226c4d
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-12-10 13:25:07 -0800

    add roles to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203210943) do
+ActiveRecord::Schema.define(version: 20141210193105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -182,6 +182,7 @@ ActiveRecord::Schema.define(version: 20141203210943) do
     t.string   "uid"
     t.string   "name"
     t.boolean  "admin"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
