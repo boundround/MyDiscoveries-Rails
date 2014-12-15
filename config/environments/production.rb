c46be2a0067484c43ee326c72433f6d0dc20f7e1
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-12-15 12:43:25 -0800

    reconfigure dalli client

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,15 +13,24 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.cache_store = :dalli_store,
-                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
-                    {:username => ENV["MEMCACHIER_USERNAME"],
-                     :password => ENV["MEMCACHIER_PASSWORD"],
-                     :failover => true,
-                     :socket_timeout => 1.5,
-                     :socket_failure_delay => 0.2
+  # config.cache_store = :dalli_store,
+  #                   (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+  #                   {:username => ENV["MEMCACHIER_USERNAME"],
+  #                    :password => ENV["MEMCACHIER_PASSWORD"],
+  #                    :failover => true,
+  #                    :socket_timeout => 1.5,
+  #                    :socket_failure_delay => 0.2
+
+  config.cache_store = :dalli_store
                     }
-  client = Dalli::Client.new(ENV["MEMCACHIER_SERVERS"],
+  # client = Dalli::Client.new(ENV["MEMCACHIER_SERVERS"],
+  #                          :value_max_bytes => 10485760)
+  client = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                           :username => ENV["MEMCACHIER_USERNAME"],
+                           :password => ENV["MEMCACHIER_PASSWORD"],
+                           :failover => true,
+                           :socket_timeout => 1.5,
+                           :socket_failure_delay => 0.2,
                            :value_max_bytes => 10485760)
 
   config.action_dispatch.rack_cache = {
