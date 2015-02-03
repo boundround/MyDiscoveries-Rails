3d96f18f13b638c38c3968aaca03edbd87d3aebc
Donovan Whitworth <donovan.whitworth@gmail.com>
2015-02-02 18:20:59 -0800

    redis env

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
-$redis = Redis.new
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+ENV["REDIS_URL"] = ENV["REDISTOGO_URL"]
 
-Soulmate.redis = $redis
+Soulmate.redis = REDIS
