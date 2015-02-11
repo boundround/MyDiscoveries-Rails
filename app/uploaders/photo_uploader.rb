949b393805f0d82496fddb8e2aa8cb13a2768a65
Donovan Whitworth <donovan.whitworth@gmail.com>
2015-02-11 15:53:49 -0800

    auto orient

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -17,6 +17,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
     "photos"
   end
 
+  process :auto_orient
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
