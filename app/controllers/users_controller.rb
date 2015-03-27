af2f6f1e65ffa0bbb028401607074acefe63df12
Donovan Whitworth <donovan.whitworth@gmail.com>
2015-03-26 17:40:31 -0700

    verify current user 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -117,7 +117,7 @@ class UsersController < ApplicationController
     end
 
     def verify_current_user
-      if current_user != User.find(params[:id]) || !current_user.try(:admin)
+      if User.find(params[:id]) != current_user || !current_user.try(:admin)
         redirect_to root_path, notice: "Not Authorized!"
       end
     end
