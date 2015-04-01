45e6309642d9e90b8d2072e19342267b9e7ff213
Donovan Whitworth <donovan.whitworth@gmail.com>
2015-03-31 17:53:20 -0700

    extend FriendlyId

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,7 @@ class Place < ActiveRecord::Base
   before_destroy :remove_from_soulmate
   before_save :check_valid_url
 
-  include FriendlyId
+  extend FriendlyId
   friendly_id :slug_candidates, :use => :slugged #show display_names in place routes
 
   scope :active, -> { where.not(subscription_level: ['out', 'draft']) }
