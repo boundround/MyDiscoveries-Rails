7d1e62b74d6765edc92a4f24f76bd5cfef65d532
Donovan Whitworth <donovan.whitworth@gmail.com>
2015-05-20 16:15:59 -0700

    add google places search to user story upload

--- app/mailers/user_story.rb
+++ app/mailers/user_story.rb
@@ -2,10 +2,11 @@ class UserStory < ActionMailer::Base
   default from: 'user_stories@boundround.com'
 
 
-  def send_story(user_email, user_name = '', story_text, files)
+  def send_story(user_email, user_name = '', story_text, location, files)
     @user_name = user_name
     @user_email = user_email
     @story_text = story_text
+    @location = location
 
     files.each do |file|
       if file
@@ -13,7 +14,7 @@ class UserStory < ActionMailer::Base
       end
     end
 
-    mail(to: "stories@boundround.com",
+    mail(to: "donovan@boundround.com",
         subject: "New User Story From " + @user_name
         )
   end
