134576dd2577a504f132717bab9aeb713f7b0f89
Donovan Whitworth <donovan.whitworth@gmail.com>
2015-07-08 20:06:15 +1000

    resourcify places, add admin roles to user edit view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,15 @@
 module UsersHelper
-
+  def list_moderator_place(role)
+    if role.resource_id
+      if role.resource_type == "Place"
+        Place.find(role.resource_id).display_name
+      elsif role.resource_type == "Area"
+        Area.find(role.resource_id).display_name
+      else
+        ""
+      end
+    else
+      ""
+    end
+  end
 end
