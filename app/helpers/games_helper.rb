547f57d0caf3ee5a7dc271f66d6e6138035ecb76
Donovan Whitworth <donovan.whitworth@gmail.com>
2015-08-07 13:47:21 +1000

    add begin rescue for games thumbnail helper

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,11 +1,15 @@
 module GamesHelper
   def game_thumbnail(game)
-    if game.game_type != 'word search' && game.thumbnail
-      game.url.match(/\=.*(\.jpg|\.png|\.svg)/i)[0].gsub("=", "")
-    elsif !game.place.photos.empty?
-      game.place.photos.first.path_url(:small)
-    else
-      ''
+    begin
+      if game.game_type != 'word search' && game.thumbnail
+        game.url.match(/\=.*(\.jpg|\.png|\.svg)/i)[0].gsub("=", "")
+      elsif !game.place.photos.empty?
+        game.place.photos.first.path_url(:small)
+      else
+        ''
+      end
+    rescue
+      ""
     end
   end
 end
