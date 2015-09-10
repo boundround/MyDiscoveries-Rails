c5258b307d982273e5946b108b613872b479e5a9
Donovan Whitworth <donovan.whitworth@gmail.com>
2015-09-10 18:15:26 +1000

    send email notification for new place created

--- app/assets/javascripts/search_suggestions.js
+++ app/assets/javascripts/search_suggestions.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
                   status: "live", place_id: place_id, user_created: true, subscription_level: "basic"}, address_components: place.address_components },
           success: function(data){
             if (data.place_id !== "error"){
-              window.location.href = data.place_id;
+              window.location.href = "/places/" + data.place_id;
             } else {
               setViewForGooglePlace(place_id, city, country, userIP);
             }
