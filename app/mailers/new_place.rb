7f58872508ea0919c2cb0835fd97f343813c9921
Donovan Whitworth <donovan.whitworth@gmail.com>
2015-09-10 19:28:59 +1000

    change notification email to JK

--- app/mailers/new_place.rb
+++ app/mailers/new_place.rb
@@ -2,7 +2,7 @@ class NewPlace < ActionMailer::Base
   default from: "website@BoundRound.com"
 
   def notification(place)
-    @email = "donovan.whitworth@gmail.com"
+    @email = "janeece@boundround.com"
     @place = place
 
     mail(to: @email, subject: "New Place Created - #{@place.display_name}, #{@place.country.display_name}")
