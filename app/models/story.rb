85458e680a4559f976a28abf67be4d1ef7d90953
Donovan Whitworth <donovan.whitworth@gmail.com>
2015-09-11 09:14:52 +1000

    order user photos in story by story_priority

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,7 @@
 class Story < ActiveRecord::Base
   belongs_to :user
   belongs_to :storiable, polymorphic: true
-  has_many :user_photos, :inverse_of => :story
+  has_many :user_photos, -> { order "story_priority ASC"}, :inverse_of => :story
 
   accepts_nested_attributes_for :user_photos
 
