14e1f31c8c1abe28be70e038f709b6bde4c38964
tbbdeveloper <chris.robertson@me.com>
2015-09-13 07:26:47 +1000

    Update to support Bing Indexing

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,27 +1,45 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://app.boundround.com/"
-SitemapGenerator::Sitemap.sitemaps_host = "http://s3.amazonaws.com/#{ENV['AS3_BUCKET_NAME']}/"
+SitemapGenerator::Sitemap.default_host = "https://app.boundround.com/"
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3.amazonaws.com/#{ENV['AS3_BUCKET_NAME']}/"
 SitemapGenerator::Sitemap.public_path = 'tmp/'
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
 
 SitemapGenerator::Sitemap.create do
   # Add all areas:
-  Area.find_each do |area|
-    if area.published_status == 'live'
-      add area_path(area), :lastmod => area.updated_at
-    end
-  end
+  # Area.find_each do |area|
+  #   if area.published_status == 'live'
+  #     add area_path(area), :lastmod => area.updated_at
+  #   end
+  # end
 
   # Add all places:
   Place.find_each do |place|
     if place.status.downcase == "live"
-      add place_path(place), :lastmod => place.updated_at
+      puts
+      video_array = Array.new
+      photo_array = Array.new
+      place.photos.find_each do |photo|
+        puts photo.path
+#        a_photo = { :loc => photo.path, :title => photo.title, :caption => photo.caption, :license => photo.credit, :geo_location => }
+        a_photo = { :loc => photo.path_url, :title => photo.title, :caption => photo.caption }
+        photo_array.push(a_photo)
+      end
+      place.videos.find_each do |video|
+        puts video.vimeo_thumbnail
+#        a_video = { :thumbnail => video.vimeo_thumbnail, :content_loc => "https://vimeo.com/"+video.vimeo_id, :tags => :title => TBD, :description => TBD, }
+        a_video = { :thumbnail_loc => video.vimeo_thumbnail, :title => place.display_name, :description => ActionView::Base.full_sanitizer.sanitize(place.description), :content_loc => "https://vimeo.com/"+video.vimeo_id.to_s }
+        video_array.push(a_video)
+      end
+
+      add place_path(place), :lastmod => place.updated_at, :images => photo_array, :videos => video_array
     end
   end
 end
 
-SitemapGenerator::Sitemap.ping_search_engines('https://s3-ap-southeast-2.amazonaws.com/brwebproduction/sitemaps/sitemap.xml.gz')
+#SitemapGenerator::Sitemap.ping_search_engines('https://s3-ap-southeast-2.amazonaws.com/brwebproduction/sitemaps/sitemap.xml.gz')
+#SitemapGenerator::Sitemap.ping_search_engines('https://s3-ap-southeast-2.amazonaws.com/#{ENV['AS3_BUCKET_NAME']}/sitemaps/sitemap.xml.gz')
+SitemapGenerator::Sitemap.ping_search_engines('https://app.boundround.com/sitemap')
 
   # Put links creation logic here.
   #
