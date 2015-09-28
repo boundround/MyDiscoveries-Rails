6e8f319ec6d2f3cc61838e5f7d3704b905819c7b
Donovan Whitworth <donovan.whitworth@gmail.com>
2015-09-28 12:39:26 +1000

    redesign user sign up form, add first name last name username and address fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,7 +85,7 @@ class User < ActiveRecord::Base
         puts auth.info
         user = User.new(
           name: auth.info.name,
-          #username: auth.info.nickname || auth.uid,
+          username: auth.info.nickname || auth.uid,
           email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
           password: Devise.friendly_token[0,20]
         )
