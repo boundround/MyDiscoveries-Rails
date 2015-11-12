59ad2ec94a81bdaace921449d92d049a4d781246
Chris Robertson <oztexan@gmail.com>
2015-11-12 08:40:32 +0000

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20151102062729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "areas", force: true do |t|
     t.string   "code"
