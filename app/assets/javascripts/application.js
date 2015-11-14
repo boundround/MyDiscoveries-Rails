9d290a9fcd17656e0f0e36b21c4d303adb5ef5d2
Donovan Whitworth <donovan.whitworth@gmail.com>
2015-11-14 13:58:12 +1100

    nil error in user points

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
+//= require jquery.ui.all
 //= require jquery-fileupload/basic
 //= require jquery-fileupload/vendor/tmpl
 //= require bootstrap-select.min
