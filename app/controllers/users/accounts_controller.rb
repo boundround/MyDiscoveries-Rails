d127cb26ece6889b470a39ea1b559e498aeb6b58
Donovan Whitworth <donovan.whitworth@gmail.com>
2016-01-11 11:26:02 +1100

    fix some spelling errors and typos for forgot username

--- app/controllers/users/accounts_controller.rb
+++ app/controllers/users/accounts_controller.rb
@@ -1,12 +1,12 @@
 class Users::AccountsController < ApplicationController
 
   def forgot_username;end
-  
+
   def send_username
-      return redirect_to(users_username_new_path, notice: 'Username can not be blank.') if params[:user][:email].blank?
+      return redirect_to(users_username_new_path, notice: 'Username cannot be blank.') if params[:user][:email].blank?
       @user = User.find_by_email(params[:user][:email])
       if @user.blank?
-          redirect_to(users_username_new_path, notice: 'Username are not valid.')
+          redirect_to(users_username_new_path, notice: 'Email is not valid.')
       else
         @username = @user.username
         @email = params[:user][:email]
