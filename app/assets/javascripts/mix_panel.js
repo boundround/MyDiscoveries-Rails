16e4f30c3c9fce8ac42466e14ac9744bace18691
Donovan Whitworth <donovan.whitworth@gmail.com>
2016-01-25 14:35:22 -0800

    add key for boundroundtest on mixpanel

--- app/assets/javascripts/mix_panel.js
+++ app/assets/javascripts/mix_panel.js
@@ -1,6 +1,6 @@
 (function(e,b){if(!b.__SV){var a,f,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
 for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=e.createElement("script");a.type="text/javascript";a.async=!0;a.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";f=e.getElementsByTagName("script")[0];f.parentNode.insertBefore(a,f)}})(document,window.mixpanel||[]);
-mixpanel.init("de052d33122e3f958caca2e9c4eef0bb");
+mixpanel.init("29fd59c6aae9e194b4072c3e0e5e2b1b");
 
 
 function mouse_click_on_middle_or_left(e)
@@ -165,7 +165,7 @@ $(document).ready(function(){
 		//track user when click user review index
 		$(document).on("mousedown", 'a[href="users/reviews"]', function(e){
 			mouse_click_on_middle_or_left(e) ? mixpanel.track("Display his reviews") : null;
-		});	
+		});
 		// track user when open leave review form
 		if ($('form[action^="/places/"][action$="/reviews"]').length)
 		{
@@ -175,7 +175,7 @@ $(document).ready(function(){
 			place_name = action_arr.pop();
 			place_name = toTitleCase(place_name.split('-').join(' '))
 			opts = opts = $.extend(options, {"place": place_name});
-			
+
 			modal = leave_review_form.parents('div.modal');
 			if (modal.length)
 			{
@@ -188,7 +188,7 @@ $(document).ready(function(){
 			}
 			leave_review_form.on("submit", function(e){
 				mixpanel.track("Submit review on "+place_name, opts);
-			});		
+			});
 		}
 		//track search result click
 
