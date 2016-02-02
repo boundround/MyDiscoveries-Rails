0d229b4bdddf0cd36c6f130c20f52b535a18f604
Donovan Whitworth <donovan.whitworth@gmail.com>
2016-02-03 09:23:29 +1100

    add bootstrap to ucg update mailer and update to send to Tash

--- app/mailers/ucg_update.rb
+++ app/mailers/ucg_update.rb
@@ -2,7 +2,7 @@ class UcgUpdate < ActionMailer::Base
   default from: "website@BoundRound.com"
 
   def send_update
-    @email = "donovan@boundround.com"
+    @email = "natasha@boundround.com"
     time = Time.now.strftime("%d/%m/%y")
     @photos = UserPhoto.where(["created_at > ?", 7.days.ago])
     @stories = Story.where(["created_at > ?", 7.days.ago])
