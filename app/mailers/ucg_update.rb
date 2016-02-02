c681281c5010df4f1e36fae88a02fb39d990cdc9
Donovan Whitworth <donovan.whitworth@gmail.com>
2016-02-03 09:37:48 +1100

    add multiple recipients for ucg update email

--- app/mailers/ucg_update.rb
+++ app/mailers/ucg_update.rb
@@ -1,12 +1,12 @@
 class UcgUpdate < ActionMailer::Base
-  default from: "website@BoundRound.com"
+  default to: "natasha@boundround.com", "janeece@boundround.com"
+          from: "website@BoundRound.com"
 
   def send_update
-    @email = "natasha@boundround.com"
     time = Time.now.strftime("%d/%m/%y")
     @photos = UserPhoto.where(["created_at > ?", 7.days.ago])
     @stories = Story.where(["created_at > ?", 7.days.ago])
     @reviews = Review.where(["created_at > ?", 7.days.ago])
-    mail(to: @email, subject: "Weekly UCG Update - #{time}")
+    mail(subject: "Weekly UCG Update - #{time}")
   end
 end
