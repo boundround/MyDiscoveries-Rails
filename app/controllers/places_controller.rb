bac016ebbcd4775d4f212edbe7f4852b20fd4620
Donovan Whitworth <donovan.whitworth@gmail.com>
2016-02-22 12:26:14 +1100

    add single subcategories table

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -764,6 +764,7 @@ class PlacesController < ApplicationController
         user_photos_attributes: [:id, :title, :path, :caption, :hero, :story_id, :priority, :user_id, :place_id, :area_id, :status, :google_place_id, :google_place_name, :instagram_id, :remote_path_url, :_destroy],
         three_d_videos_attributes: [:link, :caption, :place_id],
         category_ids: [],
+        subcategory_ids: [],
         secondary_category_ids: [],
         accessibility_category_ids: [],
         best_time_to_visit_category_ids: [],
--- db/migrate/20160216025933_create_join_table_places_subcategories.rb
+++ db/migrate/20160216025933_create_join_table_places_subcategories.rb
@@ -1,5 +1,6 @@
 class CreateJoinTablePlacesSubcategories < ActiveRecord::Migration
   def change
+    drop_table :places_subcategories
     create_join_table :places, :subcategories do |t|
       t.index [:place_id, :subcategory_id]
       t.index [:subcategory_id, :place_id]
