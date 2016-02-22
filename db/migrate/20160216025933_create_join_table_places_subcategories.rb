40cae41a1f2f25fa0ef831042e0ee59792ab8bc7
Donovan Whitworth <donovan.whitworth@gmail.com>
2016-02-22 12:32:34 +1100

    remove drop table places subcategories

--- db/migrate/20160216025933_create_join_table_places_subcategories.rb
+++ db/migrate/20160216025933_create_join_table_places_subcategories.rb
@@ -1,6 +1,5 @@
 class CreateJoinTablePlacesSubcategories < ActiveRecord::Migration
   def change
-    drop_table :places_subcategories
     create_join_table :places, :subcategories do |t|
       t.index [:place_id, :subcategory_id]
       t.index [:subcategory_id, :place_id]
