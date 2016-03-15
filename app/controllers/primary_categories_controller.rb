b2392ff771a71d2604965fe0e483c1d257fb2105
3lviend <henryss_10@yahoo.com>
2016-03-15 15:42:12 +0700

    updates story section

--- app/controllers/primary_categories_controller.rb
+++ app/controllers/primary_categories_controller.rb
@@ -1,10 +1,11 @@
 class PrimaryCategoriesController < ApplicationController
 	
 	before_action :category, only: [:show]
+	before_action :stories, only: [:show] #get stories
 
 	def show
 		@areas= category.places.is_area.paginate(per_page: 10, page: params[:areas_page])
-		@places= category.places.is_not_area.paginate(per_page: 1, page: params[:places_page])
+		@places= category.places.is_not_area.paginate(per_page: 6, page: params[:places_page])
 	end
 
 	private 
@@ -18,6 +19,5 @@ class PrimaryCategoriesController < ApplicationController
 		def stories
 			@stories ||= ApiBlog.get_cached_blogs(params[:id], "primary_cat")
 		end
-
 
 end
