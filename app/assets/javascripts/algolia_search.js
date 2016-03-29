92d25e918e982b152b389200b72ff89f2fc1136a
3lviend <henryss_10@yahoo.com>
2016-03-29 18:06:31 +0700

    updates auto complete style and rescue the image

--- app/assets/javascripts/algolia_search.js
+++ app/assets/javascripts/algolia_search.js
@@ -18,22 +18,24 @@ $(document).ready(function(){
       $('.search-results').show();
       $('.google-results-container').show();
       $('.google-results-container').append($('.pac-container'));
-      renderNoResults(content);
+      // renderNoResults(content);
       $('.br15_header').slideDown(400,function(){google.maps.event.trigger(br_map, "resize");});
       $('#br15_map').removeClass('br15_min_h_530');
       $('.br15_map').addClass('br15_collapse');
   };
 
   // $searchInput = $('#search-box');
-  $searchInput = $('#search-box');
+  $searchInput = $('.search-box');
+  $searchInputBottom = $('.search-box-bottom');
   $searchInputIcon = $('#search-button');
   $main = $('main');
   $sortBySelect = $('#sort-by-select');
   $hits = $('#hits');
   $stats = $('#stats');
   $facets = $('#facets');
-  $pagination = $('#pagination');
-  $noResult = $('#no-result');
+  // $pagination = $('#pagination');
+  // $noResult = $('#no-result');
+  $moreResult = $('#more-result');
 
   var hitTemplate = Hogan.compile($('#hit-template').text());
   var statsTemplate = Hogan.compile($('#stats-template').text());
@@ -41,6 +43,7 @@ $(document).ready(function(){
   var sliderTemplate = Hogan.compile($('#slider-template').text());
   var paginationTemplate = Hogan.compile($('#pagination-template').text());
   var noResultsTemplate = Hogan.compile($('#no-results-template').text());
+  var moreResultsTemplate = Hogan.compile($('#more-results-template').text());
 
   function searchCondition(query){
     if (query.length > 0){
@@ -61,7 +64,7 @@ $(document).ready(function(){
   $searchInput
   .on('keyup', function() {
       var query = $(this).val();
-      searchCondition(query)
+      searchCondition(query);
   })
   .bind("paste", function(){
       var elem = $(this);
@@ -71,44 +74,66 @@ $(document).ready(function(){
       }, 100);
   })
   .focus();
+  
+  // $searchInputBottom
+  // .on('keyup', function() {
+  //     var query = $(this).val();
+  //     searchCondition(query);
+  // })
+  // .bind("paste", function(){
+  //     var elem = $(this);
+  //     setTimeout(function() {
+  //       query = elem.val();
+  //       searchCondition(query)
+  //     }, 100);
+  // })
+  // .focus();
 
-  // // Search results
-  // algoliaHelper.on('result', function(content, state) {
-  //   if (content.hits.length > 0){
-  //     $noResult.hide();
-  //     $('#br15_map').addClass('br15_min_h_530');
-  //     $('.br15_map').removeClass('br15_collapse');
-  //     $('.br15_header').slideUp(400,function(){google.maps.event.trigger(br_map, "resize");});
-  //     $('.br15_search_result').show();
-  //     $('.search-results').show();
-  //     $('.search-results-container').show();
-  //     $('.google-results-container').show();
-  //     $('.google-results-container').append($('.pac-container'));
-  //     renderHits(content);
-  //     $('#magnifying-glass').on('click', function(){
-  //       var data = $('.hit-result')[0];
-  //       var link = $(data).data("link");
-  //       window.location = link;
-  //     });
+  // Search results
+  algoliaHelper.on('result', function(content, state) {
+    if (content.hits.length > 0){
+      // $noResult.hide();
+      // $('#br15_map').addClass('br15_min_h_530');
+      // $('.br15_map').removeClass('br15_collapse');
+      // $('.br15_header').slideUp(400,function(){google.maps.event.trigger(br_map, "resize");});
+      // $('.br15_search_result').show();
+      $('.search-results-container').show();
+      $('.search-results').show();
+      // $('.google-results-container').show();
+      // $('.google-results-container').append($('.pac-container'));
+      renderHits(content);
+      renderMoreResults(content);
+      rescueImage();
+      $('#magnifying-glass').on('click', function(){
+        var data = $('.hit-result')[0];
+        var link = $(data).data("link");
+        window.location = link;
+      });
 
-  //     renderStats(content);
-  //     renderPagination(content);
-  //   } else {
-  //     $('.br15_search_result').show();
-  //     $('.search-results-container').show();
-  //     $('.search-results').show();
-  //     $('.google-results-container').show();
-  //     $('.google-results-container').append($('.pac-container'));
-  //     renderNoResults(content);
-  //     $('.br15_header').slideDown(400,function(){google.maps.event.trigger(br_map, "resize");});
-  //     $('#br15_map').removeClass('br15_min_h_530');
-  //     $('.br15_map').addClass('br15_collapse');
-  //   }
-  // });
+      // renderStats(content);
+      // renderPagination(content);
+    } else {
+      $('.br15_search_result').show();
+      $('.search-results-container').show();
+      $('.search-results').show();
+      // $('.google-results-container').show();
+      // $('.google-results-container').append($('.pac-container'));
+      // renderNoResults(content);
+      // $('.br15_header').slideDown(400,function(){google.maps.event.trigger(br_map, "resize");});
+      // $('#br15_map').removeClass('br15_min_h_530');
+      // $('.br15_map').addClass('br15_collapse');
+    }
+  });
 
-  function renderNoResults(content){
-    $noResult.html(noResultsTemplate.render(content));
+  function renderMoreResults(content){
+    // alert("more result");
+    // console.log(content.query);
+    $moreResult.html(moreResultsTemplate.render(content));
   }
+  
+  // function renderNoResults(content){
+  //   $noResult.html(noResultsTemplate.render(content));
+  // }
 
   function renderHits(content) {
     $hits.html(hitTemplate.render(content));
@@ -122,131 +147,142 @@ $(document).ready(function(){
     };
     $stats.html(statsTemplate.render(stats));
   }
+function rescueImage(){
+      // $($(".hit-icon img")[0]).attr("src") == ""
+    var img = $(".hit-icon img");
+    $.each(img, function(index, val) {
+       if ($(val).attr("src") == ""){
+        // console.log($(val).attr("src") == "");
+            $(val).attr('src', "/assets/generic_hero.jpg");
+            console.log($(val).html());
+       }
+    });
+}
 
-  function renderPagination(content) {
-    var pages = [];
-    if (content.page > 3) {
-      pages.push({current: false, number: 1});
-      pages.push({current: false, number: '...', disabled: true});
-    }
-    for (var p = content.page - 3; p < content.page + 3; ++p) {
-      if (p < 0 || p >= content.nbPages) continue;
-      pages.push({current: content.page === p, number: p + 1});
-    }
-    if (content.page + 3 < content.nbPages) {
-      pages.push({current: false, number: '...', disabled: true});
-      pages.push({current: false, number: content.nbPages});
-    }
-    var pagination = {
-      pages: pages,
-      prev_page: content.page > 0 ? content.page : false,
-      next_page: content.page + 1 < content.nbPages ? content.page + 2 : false
-    };
-    $pagination.html(paginationTemplate.render(pagination));
-  }
+  // function renderPagination(content) {
+  //   var pages = [];
+  //   if (content.page > 3) {
+  //     pages.push({current: false, number: 1});
+  //     pages.push({current: false, number: '...', disabled: true});
+  //   }
+  //   for (var p = content.page - 3; p < content.page + 3; ++p) {
+  //     if (p < 0 || p >= content.nbPages) continue;
+  //     pages.push({current: content.page === p, number: p + 1});
+  //   }
+  //   if (content.page + 3 < content.nbPages) {
+  //     pages.push({current: false, number: '...', disabled: true});
+  //     pages.push({current: false, number: content.nbPages});
+  //   }
+  //   var pagination = {
+  //     pages: pages,
+  //     prev_page: content.page > 0 ? content.page : false,
+  //     next_page: content.page + 1 < content.nbPages ? content.page + 2 : false
+  //   };
+  //   $pagination.html(paginationTemplate.render(pagination));
+  // }
 
   // NO RESULTS
   // ==========
 
-  function handleNoResults(content) {
-    if (content.nbHits > 0) {
-      $main.removeClass('no-results');
-      return;
-    }
-    $main.addClass('no-results');
+  // function handleNoResults(content) {
+  //   if (content.nbHits > 0) {
+  //     $main.removeClass('no-results');
+  //     return;
+  //   }
+  //   $main.addClass('no-results');
 
-    var filters = [];
-    var i;
-    var j;
-    for (i in algoliaHelper.state.facetsRefinements) {
-      filters.push({
-        class: 'toggle-refine',
-        facet: i, facet_value: algoliaHelper.state.facetsRefinements[i],
-        label: FACETS_LABELS[i] + ': ',
-        label_value: algoliaHelper.state.facetsRefinements[i]
-      });
-    }
-    for (i in algoliaHelper.state.disjunctiveFacetsRefinements) {
-      for (j in algoliaHelper.state.disjunctiveFacetsRefinements[i]) {
-        filters.push({
-          class: 'toggle-refine',
-          facet: i,
-          facet_value: algoliaHelper.state.disjunctiveFacetsRefinements[i][j],
-          label: FACETS_LABELS[i] + ': ',
-          label_value: algoliaHelper.state.disjunctiveFacetsRefinements[i][j]
-        });
-      }
-    }
-    for (i in algoliaHelper.state.numericRefinements) {
-      for (j in algoliaHelper.state.numericRefinements[i]) {
-        filters.push({
-          class: 'remove-numeric-refine',
-          facet: i,
-          facet_value: j,
-          label: FACETS_LABELS[i] + ' ',
-          label_value: j + ' ' + algoliaHelper.state.numericRefinements[i][j]
-        });
-      }
-    }
-    $hits.html(noResultsTemplate.render({query: content.query, filters: filters}));
-  }
+  //   var filters = [];
+  //   var i;
+  //   var j;
+  //   for (i in algoliaHelper.state.facetsRefinements) {
+  //     filters.push({
+  //       class: 'toggle-refine',
+  //       facet: i, facet_value: algoliaHelper.state.facetsRefinements[i],
+  //       label: FACETS_LABELS[i] + ': ',
+  //       label_value: algoliaHelper.state.facetsRefinements[i]
+  //     });
+  //   }
+  //   for (i in algoliaHelper.state.disjunctiveFacetsRefinements) {
+  //     for (j in algoliaHelper.state.disjunctiveFacetsRefinements[i]) {
+  //       filters.push({
+  //         class: 'toggle-refine',
+  //         facet: i,
+  //         facet_value: algoliaHelper.state.disjunctiveFacetsRefinements[i][j],
+  //         label: FACETS_LABELS[i] + ': ',
+  //         label_value: algoliaHelper.state.disjunctiveFacetsRefinements[i][j]
+  //       });
+  //     }
+  //   }
+  //   for (i in algoliaHelper.state.numericRefinements) {
+  //     for (j in algoliaHelper.state.numericRefinements[i]) {
+  //       filters.push({
+  //         class: 'remove-numeric-refine',
+  //         facet: i,
+  //         facet_value: j,
+  //         label: FACETS_LABELS[i] + ' ',
+  //         label_value: j + ' ' + algoliaHelper.state.numericRefinements[i][j]
+  //       });
+  //     }
+  //   }
+  //   $hits.html(noResultsTemplate.render({query: content.query, filters: filters}));
+  // }
 
-  $(document).on('click', '.go-to-page', function(e) {
-    e.preventDefault();
-    // $('html, body').animate({scrollTop: 0}, '500', 'swing');
-    algoliaHelper.setCurrentPage(+$(this).data('page') - 1).search();
-  });
+  // $(document).on('click', '.go-to-page', function(e) {
+  //   e.preventDefault();
+  //   // $('html, body').animate({scrollTop: 0}, '500', 'swing');
+  //   algoliaHelper.setCurrentPage(+$(this).data('page') - 1).search();
+  // });
 
 //#NewCode
-function searchRequest(){
+// function searchRequest(){
 
-    $('#form-search').submit(function(event) {
-      event.preventDefault();
-      // alert("klik");
-      var query = $(this).find('input[name=query-top]').val();
-      // searchCondition(query);
-      algoliaHelper.setQuery(query);
-      algoliaHelper.search();
-    });
-      // Search results
-      algoliaHelper.on('result', function(content, state) {
-        if (content.hits.length > 0){
-          // alert("content > 0")
-          // window.location = "/results"
+//     $('#form-search').submit(function(event) {
+//       event.preventDefault();
+//       // alert("klik");
+//       var query = $(this).find('input[name=query-top]').val();
+//       // searchCondition(query);
+//       algoliaHelper.setQuery(query);
+//       algoliaHelper.search();
+//     });
+//       // Search results
+//       algoliaHelper.on('result', function(content, state) {
+//         if (content.hits.length > 0){
+//           // alert("content > 0")
+//           // window.location = "/results"
           
-          console.log(content);
-          console.log(content.index);
-          console.log(state);
+//           console.log(content);
+//           console.log(content.index);
+//           console.log(state);
 
-          var place_card = "<div class='col-xs-12 col-md-6'>"+
-                            "<div class='thumbnail'>"+
-                              "<div class='image-cat image-cat-left'>"+
-                                "<img src='./img/images.jpg' alt=' class='img-responsive img-full-div'>"+
-                              "</div>"+
-                              "<div class='caption caption-right'>"+
-                                "<h4 class='gray-font'>Sydney</h4>"+
-                                "<h2>The Powerhouse Museum</h2>"+
-                              "</div>"+
-                              "<div class='details-dest'>"+
-                                "<div class='left'>"+
-                                  "<a href='#'><i class='fa fa-male'></i> Do</a>"+
-                                  "<a href='#'>Museum</a>"+
-                                "</div>"+
-                                "<div class='right'>"+
-                                  "$$"+
-                                "</div>"+
-                                "</div>"+
-                              "</div>"+
-                            "</div>";
-      $("#area-card").append(place_card);
-      // console.log(place_card);
+//           var place_card = "<div class='col-xs-12 col-md-6'>"+
+//                             "<div class='thumbnail'>"+
+//                               "<div class='image-cat image-cat-left'>"+
+//                                 "<img src='./img/images.jpg' alt=' class='img-responsive img-full-div'>"+
+//                               "</div>"+
+//                               "<div class='caption caption-right'>"+
+//                                 "<h4 class='gray-font'>Sydney</h4>"+
+//                                 "<h2>The Powerhouse Museum</h2>"+
+//                               "</div>"+
+//                               "<div class='details-dest'>"+
+//                                 "<div class='left'>"+
+//                                   "<a href='#'><i class='fa fa-male'></i> Do</a>"+
+//                                   "<a href='#'>Museum</a>"+
+//                                 "</div>"+
+//                                 "<div class='right'>"+
+//                                   "$$"+
+//                                 "</div>"+
+//                                 "</div>"+
+//                               "</div>"+
+//                             "</div>";
+//       $("#area-card").append(place_card);
+//       // console.log(place_card);
 
-        } else {
-          // alert("content < 0")
+//         } else {
+//           // alert("content < 0")
           
-        }
-      });     
-    }
-searchRequest();
+//         }
+//       });     
+//     }
+// searchRequest();
 
 });
