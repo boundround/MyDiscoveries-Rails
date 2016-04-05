06825fe80513f1c2da3f0d6187c21915821ef711
Donovan Whitworth <donovan.whitworth@gmail.com>
2016-04-06 09:49:58 +1000

    add text for subcategories

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326052254) do
+ActiveRecord::Schema.define(version: 20160405233417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "areas", force: true do |t|
     t.string   "code"
@@ -54,6 +55,20 @@ ActiveRecord::Schema.define(version: 20160326052254) do
     t.integer  "rateable_id"
     t.string   "rateable_type"
     t.float    "avg",           null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "bootsy_image_galleries", force: true do |t|
+    t.integer  "bootsy_resource_id"
+    t.string   "bootsy_resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "bootsy_images", force: true do |t|
+    t.string   "image_file"
+    t.integer  "image_gallery_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -170,22 +185,6 @@ ActiveRecord::Schema.define(version: 20160326052254) do
     t.integer "user_id"
     t.integer "place_id"
   end
-
-  create_table "delayed_jobs", force: true do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "discounts", force: true do |t|
     t.text     "description"
@@ -448,6 +447,18 @@ ActiveRecord::Schema.define(version: 20160326052254) do
     t.datetime "updated_at"
   end
 
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "status"
+    t.string   "credit"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
+
   create_table "primary_categories", force: true do |t|
     t.string   "name"
     t.string   "identifier"
@@ -612,6 +623,8 @@ ActiveRecord::Schema.define(version: 20160326052254) do
     t.datetime "updated_at"
     t.text     "identifier"
     t.string   "icon"
+    t.text     "primary_description"
+    t.text     "secondary_description"
   end
 
   create_table "suggested_places", force: true do |t|
