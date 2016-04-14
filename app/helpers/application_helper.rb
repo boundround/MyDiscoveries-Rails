a087554e3f28b55006ff6925192e5c71a1385da9
Tamara Littlewood <you@example.com>
2016-04-14 12:34:53 +1000

    second commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,80 @@
 module ApplicationHelper
 
+  def draw_prices(prices)
+    prices_string = ""
+
+    if prices.any? {|price| price.name == "Free"}
+      prices_string += "<span class='green-font'>Free</span>"
+    else 
+      prices_string += "<span class='grey-font'>Free</span>"
+    end
+
+    if prices.any? {|price| price.name == "$"}
+      prices_string += "<span class='green-font'>$</span>"
+    else
+      prices_string += "<span class='grey-font'>$</span>"
+    end
+
+    if prices.any? {|price| price.name == "$$"}
+      prices_string += "<span class='green-font'>$$</span>"
+    else
+      prices_string += "<span class='grey-font>$$</span>"
+    end
+
+    if prices.any? {|price| price.name == "$$$"}
+      prices_string += "<span class='green-font'>$$$</span>"
+    else
+      prices_string += "<span class='grey-font'>$$$</span>"
+    end
+
+    prices_string
+
+  end
+
+  def draw_rating_smiles(place)
+    if !@place.quality_average.blank?
+      orange = ""
+      grey = ""
+      rating = place.quality_average.avg.round
+      remainder = 5 - place.quality_average.avg.round
+      rating.times do
+        orange += "<i class='fa fa-smile-o'></i>"
+      end
+
+      remainder.times do
+        grey += "<i class='fa fa-smile-o'></i>"
+      end
+
+      solution = "<span class='orange-icon'>" + orange + "</span>" + "<span class='grey-icon'>" + grey + "</span>"
+    else
+      solution = "<span class='orange-icon'><i class='fa fa-smile-o'></i><i class='fa fa-smile-o'></i><i class='fa fa-smile-o'></i><i class='fa fa-smile-o'></i><i class='fa fa-smile-o'></i></span>"
+    end
+  end
+
+  def draw_smile_review(review)
+    if Rate.find_by_rater_id_and_rateable_id(review.user.id, review.reviewable.id).stars.round > 0
+      orange = ""
+      grey = ""
+      rating = Rate.find_by_rater_id_and_rateable_id(review.user.id, review.reviewable.id).stars.round
+      remainder = 5 - Rate.find_by_rater_id_and_rateable_id(review.user.id, review.reviewable.id).stars.round
+
+      rating.times do
+        orange += "<i class='fa fa-smile-o'></i>"
+      end
+
+      remainder.times do
+        grey += "<i class='fa fa-smile-o'></i>"
+      end
+
+      solution = "<span class='orange-icon'>" + orange + "</span>" + "<span class='grey-icon'>" + grey + "</span>"
+
+    else
+      # rating = 0
+      # remainder = 5
+      solution = "<span class='grey-icon'><i class='fa fa-smile-o'></i><i class='fa fa-smile-o'></i><i class='fa fa-smile-o'></i><i class='fa fa-smile-o'></i><i class='fa fa-smile-o'></i></span>"
+    end
+  end
+
   def resource_name
     :user
   end
