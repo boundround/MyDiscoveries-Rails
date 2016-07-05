13b918f2abc0a63a0cb6473fcca7338266465367
Donovan Whitworth <donovan.whitworth@gmail.com>
2016-07-05 12:04:11 +1000

    reconfigure devise sanitized parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,11 +52,11 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |u|
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:name, :first_name, :last_name, :promo_code, :country, :username,
         :email, :password, :password_confirmation)
     end
-    devise_parameter_sanitizer.for(:account_update) do |u|
+    devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :first_name, :last_name, :promo_code, :country, :username,
         :email, :password, :password_confirmation, :current_password)
     end
