50cffb76a9307bdce50eb27c083c6688ae581ed3
Donovan Whitworth <donovan.whitworth@gmail.com>
2016-08-31 14:33:56 +1000

    redirect new users to edit profile page after sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -29,11 +29,11 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    user_path(resource)
+    edit_user_path(resource)
   end
 
   def after_inactive_sign_up_path_for(resource)
-    user_path(resource)
+    edit_user_path(resource)
   end
 
 end
