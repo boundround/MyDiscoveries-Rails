1dd0173336c82471cbd3a10e1a4696dd6889cb77
Donovan Whitworth <donovan.whitworth@gmail.com>
2016-10-05 15:38:51 +1100

    add credentials

--- config/initializers/analytics_reporting_service.rb
+++ config/initializers/analytics_reporting_service.rb
@@ -2,3 +2,13 @@ require 'google/apis/analyticsreporting_v4'
 $analytics_reporting_service = Google::Apis::AnalyticsreportingV4::AnalyticsReportingService.new
 $analytics_reporting_service.authorization = Google::Auth.get_application_default(
   ['https://www.googleapis.com/auth/analytics.readonly'])
+
+
+
+# require 'google/apis/analyticsreporting_v4'
+# scopes = ["https://www.googleapis.com/auth/analytics.readonly"]
+# auth = Google::Auth.get_application_default(scopes)
+# $analytics_reporting_service = Google::Apis::AnalyticsreportingV4::AnalyticsReportingService.new
+# #$analytics_reporting_service.authorization = Google::Auth.get_application_default(['https://www.googleapis.com/auth/analytics.readonly'])
+# $analytics_reporting_service.authorization = auth
+# $analytics_reporting_service.authorization.fetch_access_token!
