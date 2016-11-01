ced2769df7df0e27fee40448a9d526eff26421ca
3lviend <henryss10@yahoo.com>
2016-11-01 11:38:10 +0700

    fixed find children on place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -686,8 +686,10 @@ class Place < ActiveRecord::Base
   def children
     list_of_children = []
     childrens.each do |child|
-      if child.itemable_type == "Attraction" && child.itemable.status == "live"
-        list_of_children << child.itemable
+      unless child.itemable.blank?
+        if child.itemable_type == "Attraction" && child.itemable.status == "live"
+          list_of_children << child.itemable
+        end
       end
     end
     list_of_children
