944e102f997df42ff158a4e083ac59fb8ad662a8
Donovan Whitworth <donovan.whitworth@gmail.com>
2016-11-14 11:04:19 +1100

    fix path for place cards on stories show page

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   resources :stories do
     collection { get 'paginate' }
     member do
-      get 'paginate_place_to_visit'
+      get 'paginate_place'
       post :upload_image
       post :delete_image
     end
