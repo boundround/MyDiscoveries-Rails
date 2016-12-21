cf702a448d05bc23fb3ee1cfddb3cba858e8546d
3lviend <henryss_10@yahoo.com>
2016-12-21 10:45:51 +0700

    fixed slick carousel

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -104,4 +104,69 @@
   if ($(window).width() < 992) {
     createOwlCarousel();
   }
+        if($('select').length){
+            $('select').select2();
+        }
+
+         //Desktop screen customization
+        var isScreen = window.matchMedia("only screen and (min-width: 1201px)");
+
+        if(isScreen.matches) {
+
+            //Product page Gallery slider
+            if($('.slider-for').length && $('.slider-nav').length) {
+                $('.slider-for').slick({
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    fade: true,
+                    asNavFor: '.slider-nav'
+                });
+                $('.slider-nav').slick({
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    asNavFor: '.slider-for',
+                    centerMode: true,
+                    focusOnSelect: true,
+                    centerPadding: 0,
+                    arrows:false
+                });
+            }
+
+        }
+        console.log($('.slider-for'));
+        
+        //Product page accordion
+        $( "#product-accordion" ).accordion({
+            collapsible: true,
+            heightStyle: "content"
+        });
+
+        //Product page itinerary tabs
+
+        if($('#itinerary-tabs').length){
+            $( "#itinerary-tabs" ).tabs({
+                active: 0
+            });
+        }
+
+        //Selection page datepicker
+        if($('#departure-date').length) {
+            $( "#departure-date" ).datepicker();
+        }
+
+
+        //User pages page datepicker
+        if($('#up-settings-dob').length) {
+            $( "#up-settings-dob" ).datepicker();
+        }
+
+        //User pages tabs
+        if($('#up-tabs').length) {
+            $( "#up-tabs" ).tabs();
+        }
+
+        if($('#up-mc-tabs').length) {
+            $( "#up-mc-tabs" ).tabs();
+        }
 });
