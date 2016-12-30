b71f38ad4dcd73173c95c76ff765fc0b34c01ea8
3lviend <henryss_10@yahoo.com>
2016-12-30 15:04:51 +0700

    to explain how to update joining table

--- app/models/child_item.rb
+++ app/models/child_item.rb
@@ -2,17 +2,20 @@ class ChildItem < ActiveRecord::Base
   belongs_to :itemable, :polymorphic => true
   belongs_to :parentable, :polymorphic => true
 
+  #we save the relations to hbtm table after created ChildItem record
   after_create :split_child_item
 
   def split_child_item
     if self.parentable_type == "Region"
-      parent = self.parentable #Region will be the parent
-      child = self.itemable #Child item, it can be Place or Attraction
-      child.regions = [parent] #Storing data to join table, can be attractions_regions or places_regions
+      #if the parent of ChildItem is Region 
+      # we create a child related to region(parent) 
+      # to join table attraction_regions or places_regions with the method below 
+      self.itemable.regions = [self.parentable]
     elsif self.parentable_type == "Place"
-      parent = self.parentable #Place will be the parent
-      child = self.itemable #Child item, will be Attaction
-      child.places = [parent] #Storing data to join table attractions_places
+      #if the parent of ChildItem is Place 
+      # we create a child related to place(parent) 
+      # to join table attraction_places with the method below 
+      self.itemable.places = [self.parentable]
     end
   end
 end
