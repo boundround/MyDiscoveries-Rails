0c8392d0f1b2ff27e5448cdbb1eb29007ade57fb
3lviend <henryss_10@yahoo.com>
2017-01-09 11:00:51 +0700

    modify path after sign in to page user current views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,7 +51,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-   session[:previous_url] || root_path
+    URI(request.referer || '').path
   end
 
   def configure_permitted_parameters
