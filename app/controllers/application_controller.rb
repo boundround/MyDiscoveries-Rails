6c246412403c065dc1e378e77b1971cc2db7fe51
3lviend <henryss_10@yahoo.com>
2017-01-17 09:47:31 +0700

    change redirect after sign in to users#show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,7 +51,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    URI(request.referer || '').path
+    resource
+    # URI(request.referer || '').path
   end
 
   def configure_permitted_parameters
