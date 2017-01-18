9f083219772860f312f0a178f6a9309886912062
Donovan Whitworth <donovan.whitworth@gmail.com>
2017-01-19 06:31:48 +1100

    remove attractions from region model

--- app/models/region.rb
+++ app/models/region.rb
@@ -134,7 +134,7 @@ class Region < ActiveRecord::Base
   accepts_nested_attributes_for :videos, allow_destroy: true
   accepts_nested_attributes_for :fun_facts, allow_destroy: true
   accepts_nested_attributes_for :stories, allow_destroy: true
-  before_save :fill_places, :fill_attractions
+  before_save :fill_places
 
 
   def get_parents(region, parents = [])
@@ -183,19 +183,7 @@ class Region < ActiveRecord::Base
   end
 
   def get_attractions
-    places_list = []
-    queue = self.children
 
-    while !queue.empty?
-    place = queue.shift
-      if place.class.to_s == "Attraction" && place.status == "live"
-        places_list << place
-      end
-      place.children.each do |child|
-        queue << child
-      end
-    end
-    places_list
   end
 
   def fill_places
@@ -204,66 +192,11 @@ class Region < ActiveRecord::Base
   end
 
   def fill_attractions
-    self.attractions = []
-    self.attractions = self.get_attractions
+
   end
 
   def all_place_children
-    Rails.cache.fetch([self, "all_place_children"]) do
-      childrens_collect = self.places
-      data_marker = []
-      if !childrens_collect.blank?
-        childrens_collect.each do |place|
-          data_objs = {}
-          data_objs['#place'] = place.display_name
-          data_objs['#lat'] = place.latitude
-          data_objs['#lng'] = place.longitude
-          data_objs['#description'] = place.description
-          data_objs['#country'] = place.country.display_name rescue ""
-          data_objs['#path'] = place_path(place)
-          if !place.photos.blank?
-            data_objs['#photo'] = place.photos.last.path_url(:thumb)
-          else
-            data_objs['#photo'] = "/assets/generic-hero-thumb.jpg"
-          end
 
-          data_objs['#childrens'] = []
-          place.attractions.each do |place_child|
-              place_item_child = place_child
-              data_child_objs = {}
-              data_child_objs['@country_child'] = place_item_child.country.display_name rescue ""
-              data_child_objs['@name_child'] = place_item_child.display_name
-              if !place_item_child.photos.blank?
-                data_child_objs['@photo_child'] = place_item_child.photos.last.path_url(:thumb)
-              else
-                data_child_objs['@photo_child'] = "/assets/generic-hero-thumb.jpg"
-              end
-              data_objs['#childrens'] << data_child_objs
-              data_objs['#childrens'] << ["#"]
-          end
-          data_marker << data_objs
-          data_marker << ["@"]
-        end
-        return data_marker
-      else
-        data_objs = {}
-        data_objs['#place'] = self.display_name
-        data_objs['#lat'] = self.latitude.to_f
-        data_objs['#lng'] = self.longitude.to_f
-        data_objs['#description'] = self.description
-        data_objs['#country'] = ''
-        data_objs['#path'] = place_path(self)
-        if !self.photos.blank?
-          data_objs['#photo'] = self.photos.last.path_url(:thumb)
-        else
-          data_objs['#photo'] = "/assets/generic-hero-thumb.jpg"
-        end
-        data_objs['#childrens'] = []
-      end
-      data_marker << data_objs
-      data_marker << ["@"]
-      return data_marker
-    end
   end
 
   private
