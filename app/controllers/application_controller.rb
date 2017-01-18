7b818e64875010742f924cbb5e5e1ae8c5d7ea6e
3lviend <henryss_10@yahoo.com>
2017-01-18 10:03:36 +0700

    fixed user path after sign_in and sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,8 +51,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    resource
-    # URI(request.referer || '').path
+    URI(request.referer || '').path
   end
 
   def configure_permitted_parameters
