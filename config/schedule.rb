d64b3b9e48cd46d28b1c0340c52f78510ed2267c
Donovan Whitworth <donovan.whitworth@gmail.com>
2017-02-27 17:55:10 +1100

    add pinterest tracker

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,1 @@
-every :day do
-  rake 'places:publish', environment: environment
-  if ENV['BOUNDROUND_ENV'] == 'boundround_production'
-    rake 'sitemap:refresh'
-    rake 'page_ranking_weight:update'
-  end
-end
 
-every :monday, :at => '12pm' do
-  rake 'weekly_ucg:send_ucg_update', environment: environment
-end
