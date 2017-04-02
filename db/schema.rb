ffbc8bc2b0e9e510649e491138e872a84b1c5e0e
Donovan Whitworth <donovan.whitworth@gmail.com>
2017-04-03 09:51:20 +1000

    add rake task to copy supplier product codes

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316094921) do
+ActiveRecord::Schema.define(version: 20170316112629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "areas", force: true do |t|
     t.string   "code"
