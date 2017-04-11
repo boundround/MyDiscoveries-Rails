2953eb0ffd0d2e99b4ec15360572c1dd019d1975
Donovan Whitworth <donovan.whitworth@gmail.com>
2017-04-11 10:44:16 +1000

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,5 +109,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # config.force_ssl = true
-  GA.tracker = "UA-97088347-1"
+  GA.tracker = "UA-91823234-1"
 end
