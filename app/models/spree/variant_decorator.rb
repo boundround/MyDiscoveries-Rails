1b6d9f565cdb97bd1a3c59e20159ba2a58a0858e
Donovan Whitworth <donovan.whitworth@gmail.com>
2017-04-22 01:42:31 +1000

    add tripe to bed_type enum

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Variant.class_eval do
 
   enum maturity: { adult: 0, child: 1 } unless instance_methods.include? :maturity
-  enum bed_type: { single: 0, twin: 1 } unless instance_methods.include? :bed_type
+  enum bed_type: { single: 0, twin: 1, triple: 2 } unless instance_methods.include? :bed_type
 
   # overrides default getter
   def description
