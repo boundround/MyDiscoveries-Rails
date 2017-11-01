93f2694fed87f15434bc7b1a05a4a7b6dc3005f7
Surya Setiyaputra <hello@setiyaputra.me>
2017-11-01 12:46:19 +1100

    fix error story_policy.rb

--- app/policies/story_policy.rb
+++ app/policies/story_policy.rb
@@ -1,9 +1,33 @@
 class StoryPolicy < ApplicationPolicy
-  def seo_analysis?
+  def index?
     if @user.blank?
       false
     else
       @user.admin?
     end
+  end
+
+  def create?
+    index?
+  end
+
+  def new?
+    index?
+  end
+
+  def update?
+    index?
+  end
+
+  def edit?
+    index?
+  end
+
+  def destroy?
+    index?
+  end
+
+  def seo_analysis?
+    index?
   end
 end
