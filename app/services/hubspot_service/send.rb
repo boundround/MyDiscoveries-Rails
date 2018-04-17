0a0e99f4bcc2cbce2fa392b4e69dbb7bbaa1cb94
Donovan Whitworth <donovan.whitworth@gmail.com>
2018-04-17 13:46:56 +1000

    fix rubocop errors

--- app/services/hubspot_service/send.rb
+++ app/services/hubspot_service/send.rb
@@ -23,23 +23,23 @@ class HubspotService::Send
       deal = Hubspot::Deal.find(deal_id)
       response = deal.update!(
       {
-          "amount" =>  order.total, 
-          "description" => order.line_items[0].variant.product.description,
-          "dealname" => order.line_items[0].variant.product.name,
-          "dealstage" => dealstage,
-          "holiday_type" => 'holiday',
-          "pipeline" => ENV['HUBSPOT_PIPELINE_ID'],
-          "hubspot_owner_id" => ENV['HUBSPOT_OWNER_ID'],
-          "bed_type" => order.line_items[0].variant.bed_type,
-          "date_variant" => order.line_items[0].variant.departure_date,
-          "departure_city" => order.line_items[0].variant.departure_city,
-          "duration_days_" => order.line_items[0].variant.product.number_of_days,
-          "hotel_accommodation" => order.line_items[0].variant.accommodation,
-          "instalments_selected" => order.line_items[0].request_installments,
-          "md_operator" => order.line_items[0].variant.product.operator.name,
-          "purchase_quantity" => order.line_items.length,
-          "supplier_product_code" => order.line_items[0].variant.supplier_product_code,
-          "order_info_link" => Rails.application.routes.url_helpers.order_customer_info_path(order, :host => "www.mydiscoveries.com.au")
+          'amount' =>  order.total, 
+          'description' => order.line_items[0].variant.product.description,
+          'dealname' => order.line_items[0].variant.product.name,
+          'dealstage' => dealstage,
+          'holiday_type' => 'holiday',
+          'pipeline' => ENV['HUBSPOT_PIPELINE_ID'],
+          'hubspot_owner_id' => ENV['HUBSPOT_OWNER_ID'],
+          'bed_type' => order.line_items[0].variant.bed_type,
+          'date_variant' => order.line_items[0].variant.departure_date,
+          'departure_city' => order.line_items[0].variant.departure_city,
+          'duration_days_' => order.line_items[0].variant.product.number_of_days,
+          'hotel_accommodation' => order.line_items[0].variant.accommodation,
+          'instalments_selected' => order.line_items[0].request_installments,
+          'md_operator' => order.line_items[0].variant.product.operator.name,
+          'purchase_quantity' => order.line_items.length,
+          'supplier_product_code' => order.line_items[0].variant.supplier_product_code,
+          'order_info_link' => Rails.application.routes.url_helpers.order_customer_info_path(order, host: 'www.mydiscoveries.com.au')
         })
       return response.deal_id
     else  
@@ -47,23 +47,23 @@ class HubspotService::Send
         ENV['HUBSPOT_PORTAL_ID'], 
         ENV['HUBSPOT_COMPANY_ID'], 
         hubspot_id,{
-          "amount" =>  order.total, 
-          "description" => order.line_items[0].variant.product.description,
-          "dealname" => order.line_items[0].variant.product.name,
-          "dealstage" => dealstage,
-          "holiday_type" => 'holiday',
-          "pipeline" => ENV['HUBSPOT_PIPELINE_ID'],
-          "hubspot_owner_id" => ENV['HUBSPOT_OWNER_ID'],
-          "bed_type" => order.line_items[0].variant.bed_type,
-          "date_variant" => order.line_items[0].variant.departure_date,
-          "departure_city" => order.line_items[0].variant.departure_city,
-          "duration_days_" => order.line_items[0].variant.product.number_of_days,
-          "hotel_accommodation" => order.line_items[0].variant.accommodation,
-          "instalments_selected" => order.line_items[0].request_installments,
-          "md_operator" => order.line_items[0].variant.product.operator.name,
-          "purchase_quantity" => order.line_items.length,
-          "supplier_product_code" => order.line_items[0].variant.supplier_product_code,
-          "order_info_link" => Rails.application.routes.url_helpers.order_customer_info_path(order, :host => "www.mydiscoveries.com.au")
+          'amount' =>  order.total, 
+          'description' => order.line_items[0].variant.product.description,
+          'dealname' => order.line_items[0].variant.product.name,
+          'dealstage' => dealstage,
+          'holiday_type' => 'holiday',
+          'pipeline' => ENV['HUBSPOT_PIPELINE_ID'],
+          'hubspot_owner_id' => ENV['HUBSPOT_OWNER_ID'],
+          'bed_type' => order.line_items[0].variant.bed_type,
+          'date_variant' => order.line_items[0].variant.departure_date,
+          'departure_city' => order.line_items[0].variant.departure_city,
+          'duration_days_' => order.line_items[0].variant.product.number_of_days,
+          'hotel_accommodation' => order.line_items[0].variant.accommodation,
+          'instalments_selected' => order.line_items[0].request_installments,
+          'md_operator' => order.line_items[0].variant.product.operator.name,
+          'purchase_quantity' => order.line_items.length,
+          'supplier_product_code' => order.line_items[0].variant.supplier_product_code,
+          'order_info_link' => Rails.application.routes.url_helpers.order_customer_info_path(order, host: 'www.mydiscoveries.com.au')
         })
       return response.deal_id
     end
@@ -71,4 +71,4 @@ class HubspotService::Send
   end
 end
 
-#"pipeline" => "mdfulfilmentpipeline" f1eeb762-48e2-4150-ae79-81075972fb10
+#'pipeline' => 'mdfulfilmentpipeline' f1eeb762-48e2-4150-ae79-81075972fb10
